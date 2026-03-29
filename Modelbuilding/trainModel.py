234fef60dcc6fd81d6fdb41190c580b943651009
Kasaksaxena <kasaksaxena6@gmail.com>
2026-03-30 00:24:53 +0530

    train model learning

---
+++ Modelbuilding/trainModel.py
@@ -0,0 +1,145 @@
+import torch
+from torch import nn 
+import matplotlib.pyplot as plt
+
+#Data preparing and loading 
+
+# Create *known* parameters
+weight = 0.7 
+bias = 0.3 
+
+#create data
+start =0
+end = 1
+step = 0.02
+
+X = torch.arange(start,end,step).unsqueeze(dim = 1)
+Y = weight * X + bias
+print(X[:10])
+print(Y[:10])
+
+# Create train/test split
+train_split = int( 0.8 * len(X))
+X_train, Y_train = X[:train_split], Y[:train_split]
+X_test, Y_test = X[train_split :], Y[train_split :]
+print(len(X_train))
+print(len(Y_train))
+print(len(X_test))
+print(len(Y_test))
+
+def plot_predictions(train_data=X_train, train_label=Y_train,
+                     test_data=X_test, test_label=Y_test,
+                     predictions=None):
+  """
+  Plots training data, test data and compares predictions.
+  """
+  plt.figure(figsize=(10,7))
+    # Plot training data in blue
+  plt.scatter(train_data ,train_label , c ="b" , s= 4 , label = "Training Data")
+  plt.scatter(test_data ,test_label , c ="g" , s= 4 , label = "Testing Data")
+  if predictions is not None:
+    # Plot the predictions in red (predictions were made on the test data)
+    plt.scatter(test_data, predictions, c="r", s=4, label="Predictions")
+
+  # Show the legend
+  plt.legend(prop={"size": 14})
+  plt.show()
+  
+plot_predictions()
+
+# Create a Linear Regression model class
+
+class LinearRegressionModel(nn.Module):
+  def __init__(self):
+    super().__init__()
+    self.weights = nn.Parameter(torch.randn(1,dtype= torch.float) , requires_grad=True)
+    self.bias = nn.Parameter(torch.randn(1,dtype= torch.float),  requires_grad=True)
+    
+      # Forward defines the computation in the model
+  
+  def forward(self , x: torch.tensor)->torch.tensor :
+    return self.weights * x + self.bias
+  
+
+torch.manual_seed(44)
+model_1 = LinearRegressionModel()
+print(list(model_1.parameters()))
+
+# Make predictions with model
+with torch.inference_mode():
+      y_preds = model_1(X_test)
+      
+# Check the predictions
+print(f"Number of testing samples: {len(X_test)}") 
+print(f"Number of predictions made: {len(y_preds)}")
+print(f"Predicted values:\n{y_preds}")
+plot_predictions(predictions=y_preds)
+
+# Training start here
+
+loss_fn = torch.nn.L1Loss()
+optimizer = torch.optim.Adam(params= model_1.parameters() , lr= 0.01)
+
+#Building a training (and testig) loop
+torch.manual_seed(44)
+epochs = 10
+train_loss_values = []
+test_loss_values = []
+epoch_count = []
+
+for epoch in range(epochs):
+    model_1.train()
+    y_preds= model_1(X_train) # forw pass
+    loss = loss_fn(y_preds,Y_train)
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    
+    model_1.eval()
+    
+    with torch.inference_mode():
+      # 1. Forward pass on test data
+      test_pred = model_1(X_test)
+
+      # 2. Caculate loss on test data
+      test_loss = loss_fn(test_pred, Y_test.type(torch.float)) # predictions come in torch.float datatype, so comparisons need to be done with tensors of the same type
+
+      if epochs % 2 ==0:
+            epoch_count.append(epoch)
+            train_loss_values.append(loss.detach().numpy())
+            test_loss_values.append(test_loss.detach().numpy())
+            print(f"Epoch: {epoch} | MAE Train Loss: {loss} | MAE Test Loss: {test_loss} ")
+
+    
+plt.plot(epoch_count,train_loss_values, label = 'Train loss')
+plt.plot(epoch_count,test_loss_values, label = 'Test loss')
+plt.title("Training and test loss curves")
+plt.ylabel("Loss")
+plt.xlabel("Epochs")
+plt.legend()
+plt.show()
+
+# Find our model's learned parameters
+print("The model learned the following values for weights and bias:")
+print(model_1.state_dict())
+print("\nAnd the original values for weights and bias are:")
+print(f"weights: {weight}, bias: {bias}")
+
+# 1. Set the model in evaluation mode
+model_1.eval()
+
+# 2. Setup the inference mode context manager
+with torch.inference_mode():
+  # 3. Make sure the calculations are done with the model and data on the same device
+  # in our case, we haven't setup device-agnostic code yet so our data and model are
+  # on the CPU by default.
+  # model_0.to(device)
+  # X_test = X_test.to(device)
+  y_preds = model_1(X_test)
+print(y_preds)
+  
+  
+
+
+
+
